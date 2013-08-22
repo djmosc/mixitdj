13be62d91751e2e1c378a3318ab030fb4f859af5
DJ Mosca <djmosc@gmail.com>
2013-08-22 14:03:50 +0100

    A few changes to the footer

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="bottom">
+		<!--div class="bottom">
 			<div class="container inner">
 				<div class="break-on-tablet span three alpha">
 					<ul class="social-links">
@@ -41,23 +41,21 @@
 					</p>
 				</div>
 			</div>
-		</div>
+		</div-->
 	</footer><!-- #footer .site-footer -->
 </div><!-- #wrap -->
 
 <?php wp_footer(); ?>
 
 <script>
-  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  // ga('create', 'UA-9989995-2', 'charlie-may.co.uk');
-  // ga('send', 'pageview');
+  ga('create', 'UA-43314623-1', 'mixitdj.co.uk');
+  ga('send', 'pageview');
 
 </script>
-<script src="//s7.addthis.com/js/300/addthis_widget.js"></script>
-
 </body>
 </html>
